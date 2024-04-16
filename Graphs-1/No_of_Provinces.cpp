60e80654287ce3078bc4cf924e4cfdbf21af6700
TanviSaxena03 <155883356+TanviSaxena03@users.noreply.github.com>
2024-04-17 00:23:16 +0530

    Create No_of_Provinces.cpp

---
+++ Graphs-1/No_of_Provinces.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(unordered_map<int,bool>& vis,int src,vector<vector<int>>& isConnected){
+        vis[src]=true;
+        int size=isConnected[src].size();
+        for(int col=0;col<size;col++){
+            if(src!=col && isConnected[src][col]==1 && !vis[col]){
+                dfs(vis,col,isConnected);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int,bool> vis;
+        int n=isConnected.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(vis,i,isConnected);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
